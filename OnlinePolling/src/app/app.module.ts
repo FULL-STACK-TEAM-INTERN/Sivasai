71a2b8d74cc5438561d8af920d02e65a21a2f71a
SivaSaiReddy <sivasaireddy07@gmail.com>
2021-04-15 09:44:03 +0530

    First Commit

---
+++ OnlinePolling/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+
+  ],
+  providers: [
+    {provide:LocationStrategy, useClass:PathLocationStrategy}
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ OnlinePolling/src/app/login/login.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  user = new User();
+
+  constructor() { }
+
+  ngOnInit(): void {
+   
+  }
+
+  
+
+}
